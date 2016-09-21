247ae75ee9d2bae46fb995d4c0514ac59d62f860
aMotahedin <aMotahedin@amotahedinsp3.bayviewglen.ca>
2016-09-21 11:06:03 -0400

    sept 21 example 1

--- Unit One/src/com/bayviewglen/dayseven/ExampleOne.java
+++ Unit One/src/com/bayviewglen/dayseven/ExampleOne.java
@@ -13,8 +13,10 @@ public class ExampleOne {
 
 		System.out.println("The numvber of secconds in a year is(wait for it...): " + seccondsPerYear);
 
-		final int SECCONDS_PER_MINUTE = 60; // declared varibles but these
-											// varibles should NEVER change
+		////////////////////////////////////////////////////////////////////////////////////////////
+		
+		final int SECCONDS_PER_MINUTE = 60; 
+											
 		final int MINUTES_PER_HOUR = 60;
 		final int HOURS_PER_DAY = 24;
 
@@ -22,5 +24,13 @@ public class ExampleOne {
 
 		System.out.println("The numvber of secconds in a year is(wait for it...): " + SECCONDS_PER_YEAR);
 
+		///////////////////////////////////////////////////////////////////////////////////////////
+		double radious = 4.2;
+		double area = Math.PI * Math.pow(radious, 2);
+		// math.random(); is a random number between 0 and 0.99999 including decimals 
+		//(math.random() +1) is 1 to 1.999999
+		// (int) 10 * (math.random()+1) is 1 to 10
+		//- 10 to 10 is 21 (int) (numbers so 21*math.random()-10)
+		
 	}
 }
