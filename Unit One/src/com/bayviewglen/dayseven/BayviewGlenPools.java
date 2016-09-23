5a10cff69f7a04e5c691e3994bb07239e7416282
aMotahedin <aMotahedin@amotahedinsp3.bayviewglen.ca>
2016-09-22 20:03:17 -0400

    Finished

--- Unit One/src/com/bayviewglen/dayseven/BayviewGlenPools.java
+++ Unit One/src/com/bayviewglen/dayseven/BayviewGlenPools.java
@@ -22,59 +22,71 @@ public class BayviewGlenPools {
 
 		System.out
 				.println("Please enter the length of the transition between the deep and shallow ends of the pool:  ");
-		double transitionhyp = number.nextDouble();
+		double transitionHyp = number.nextDouble();
 
 		System.out.println("Then enter the length of the shallow end: ");
 		double shallowLength = number.nextDouble();
 
 		System.out.println("Finally, enter the price of liner one, two and three: ");
 		double linerOne = number.nextDouble();
-
-		System.out.println("Finally, enter the price of liner two: ");
 		double linerTwo = number.nextDouble();
-
-		System.out.println("Finally, enter the price of liner Three: ");
 		double linerThree = number.nextDouble();
 
-		// question one 
-		
+		// question one
+
 		double transitionHeight = deepDepth - shallowDepth;
 
-		double transitionLenghtPt1 = transitionhyp * transitionhyp - (transitionHeight * transitionHeight);
+		double transitionLenghtPt1 = transitionHyp * transitionHyp - (transitionHeight * transitionHeight);
 		double transitionLength = Math.sqrt(transitionLenghtPt1);
 
 		double shallowVolume = shallowDepth * shallowLength * width;
 
 		double lengthDeepEnd = length - shallowLength - transitionLength;
 
-		double deepVolume = lengthDeepEnd * deepDepth * width; 
-		
+		double deepVolume = lengthDeepEnd * deepDepth * width;
+
 		double middleVolume = shallowDepth * transitionLength * width;
-		
-		double transitionTriangleHeight = deepDepth - shallowDepth; 
+
+		double transitionTriangleHeight = deepDepth - shallowDepth;
 		double transitionTriangleVolume = ((transitionTriangleHeight * transitionLength) * 0.5) * width;
-		
-		double totalVolume = transitionTriangleVolume + middleVolume + deepVolume; 
-		
-		double water = totalVolume * 0.9; 
-		
+
+		double totalVolume = transitionTriangleVolume + middleVolume + deepVolume;
+
+		double water = totalVolume * 0.9;
+
+		double waterRounded = (int) (water * 100000) / 1000.0;
+
+		System.out.println("\t");
 		System.out.println("you will need " + water + " litres of water to fill the pool 90%");
-		
-		//question two
-		
-double surfaceAreaBottom = (transitionhyp + lengthDeepEnd + shallowLength) * width; 
-		
-		double surfaceAreaFront = width * shallowDepth; 
+
+		// question two
+
+		double surfaceAreaBottom = (transitionHyp + lengthDeepEnd + shallowLength) * width;
+
+		double surfaceAreaFront = width * shallowDepth;
 		double surfaceAreaBack = width * deepDepth;
-		
+
 		double surfaceAreaDeepSides = (deepDepth * lengthDeepEnd) * 2;
 		double surfaceAreaShallowSides = (shallowLength * shallowDepth) * 2;
-		double surfaceAreaTransionBox = (shallowDepth * transitionLength) * 2; 
+		double surfaceAreaTransionBox = (shallowDepth * transitionLength) * 2;
 		double surfaceAreaTransitionTriangle = ((transitionLength * transitionTriangleHeight) / 2) * 2;
-		double surfaceAreaTotal = surfaceAreaDeepSides + surfaceAreaShallowSides + surfaceAreaTransionBox + surfaceAreaTransitionTriangle + surfaceAreaFront + surfaceAreaBack + surfaceAreaBottom;
+		double surfaceAreaTotal = surfaceAreaDeepSides + surfaceAreaShallowSides + surfaceAreaTransionBox
+				+ surfaceAreaTransitionTriangle + surfaceAreaFront + surfaceAreaBack + surfaceAreaBottom;
+
+
 		System.out.println("\t");
 		System.out.println(surfaceAreaTotal + " meters squared is needed to line this pool");
-		
+
+		// question 3
+
+		double linerOneTotal = linerOne * surfaceAreaTotal;
+		double linerTwoTotal = linerTwo * surfaceAreaTotal;
+		double linerThreeTotal = linerThree * surfaceAreaTotal;
+
+		System.out.println("\t");
+		System.out.println("price of liner one: $" + linerOneTotal);
+		System.out.println("price of liner two: $" + linerTwoTotal);
+		System.out.println("price of liner three: $" + linerThreeTotal);
 
 	}
 
